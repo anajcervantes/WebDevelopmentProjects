d0dce343501f6b93f8aabbc53c4bae59860a1fb0
anajcervantes <anitajgr@gmail.com>
2025-06-26 15:57:37 -0400

    Added listeners for each drum button

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,9 @@
+//document.querySelector("button").addEventListener("click", handleClick)
+
+for(var i=0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+function handleClick(){
+    alert("I was clicked!");
+}
